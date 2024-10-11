f22614621dbd2fb52c4d2c7a3804ec5cd2c32721
Sukish17 <165570197+Sukish17@users.noreply.github.com>
2024-10-11 19:11:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors(
 ));
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://sukishkohli:Sukish17@cluster0.dqpo9.mongodb.net/DB?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect('mongodb+srv://sukishkohli:Sukish17@cluster0.dqpo9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 
 app.get("/", (req, res) => {
